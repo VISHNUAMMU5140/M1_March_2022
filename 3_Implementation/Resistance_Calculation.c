a734a8df46d4bab8b9c52ed458910197c827ad03
VISHNUAMMU5140 <vs6562@srmist.edu.in>
2022-03-27 14:25:16 +0530

    program changed

--- 3_Implementation/Resistance_Calculation.c
+++ 3_Implementation/Resistance_Calculation.c
@@ -1,8 +1,4 @@
-//This is a program to find series and parallel resistance
-
-#include <stdio.h>
-
-// Function to calculate series resistance
+#include "stdio.h"
 float series_resistance(int res1[],int num1)
     { 
         int Rs=0;
@@ -12,59 +8,60 @@ float series_resistance(int res1[],int num1)
         }
         return Rs;
     }
-// Function to calculate parallel resistance
-float parallel_resistance(int res2[],int num2)
+
+static float parallel_resistance(int res2[],int num2)
     {   
-        int Rp=0;
+        float Rp;
+        static float temp;
         for (int i = 0; i < num2; i++)
         {
-            Rp = (Rp + (1.0 / res2[i]));
+        Rp = Rp + (1.0 / res2[i]);
+        temp = Rp; 
         }
-    Rp=1/Rp;
-    return Rp;
+    temp = 1/temp;
+    return temp;
     } 
 int main()
 {   
-    int x;   // declaring the variable for switch case
-    float ans; // variable to return the total resistance value
+    int x;   
+    float ans; 
     printf("Press 1 for Series and 2 for parallel : ");
     scanf("%d",&x);
 
 if(x==1)
 {   
-    // series case
+    
     int r1[10], n1, i, Rs = 0;
-    printf("Enter the number of Resistances : ");
+    printf("Enter the number of Resistances :  ");
     scanf("%d", &n1);
-    printf("\nEnter Value of Each Resistance : n");
+    printf("\nEnter Value of Each Resistance : ");
     for (i = 0; i < n1; i++) 
     {
         printf("\nR%d : ", i + 1);
         scanf("%d", &r1[i]);
     }
     ans=series_resistance(r1,n1);
-    printf("The total resistance is: %.3f",ans);
+    printf("The total resistance is: %.3f\n",ans);
     
 }
 else if (x==2)
 {
-    //parallel case
-    int r2[10], n2, i, Rp = 0;
+   
+    int r2[10], n2, i;
+    float Rp;
     printf("Enter the number of Resistances : ");
     scanf("%d", &n2);
-    printf("\nEnter Value of Each Resistance : n");
+    printf("\nEnter Value of Each Resistance : ");
     for (i = 0; i < n2; i++) 
     {
         printf("\nR%d : ", i + 1);
         scanf("%d", &r2[i]);
     }
     ans=parallel_resistance(r2,n2);
-    printf("The total resistance is: %.6f",ans);
+    printf("The total resistance is: %.6f\n",ans);
     
 }
 else
 printf("Invalid input");
 return 0;
 }
-
-void test_
